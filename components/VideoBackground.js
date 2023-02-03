f33a64e43295d9823775ec6355a8e5685cc0f270
Al Helal Abdullah <alhelal@Als-MacBook-Air.local>
2023-02-03 22:34:10 +0600

    corrected iphone video

--- components/VideoBackground.js
+++ components/VideoBackground.js
@@ -3,14 +3,21 @@ import React from "react";
 function VideoBackground() {
   return (
     <div className="relative w-full h-48 lg:h-[30rem] overflow-hidden bg-black bg-opacity-80">
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `
       <video
         autoPlay
         loop
         muted
+        playsinline
         width="100%"
         height="80%"
-        src="videos/bgcover.webm"
+        src="videos/bgcover.mp4"
         type="video/mp4"
+        className="absolute"
+      /> `,
+        }}
       />
     </div>
   );
